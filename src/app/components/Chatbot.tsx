6950a1316b68f90258f47ab7a00ad59f848808ff
ahmadmidlaj313@gmail.com <you@example.com>
2024-05-05 22:39:00 +0530

    few changes

--- src/app/components/Chatbot.tsx
+++ src/app/components/Chatbot.tsx
@@ -1,37 +1,38 @@
 
-// 'use client'
-// import { Input } from "@/components/ui/input"
-// import { Button } from "@/components/ui/button"
-// import { useChat } from "ai/react";
+'use client'
+import { Input } from "../components/ui/input"
+import { Button } from "../components/ui/button"
+import { useChat } from "ai/react";
 
-// export function Chatbot() {
-//   const {messages, input, handleInputChange, handleSubmit} = useChat()
+export function Chatbot() {
+  const {messages, input, handleInputChange, handleSubmit} = useChat()
+  console.log(messages);
 
-//   return (
-//     <div className="h-96 w-full max-w-[700px] ">
-//       <div className="flex flex-col h-full ">
+  return (
+    <div className="h-96 w-full max-w-[700px] ">
+      <div className="flex flex-col h-full ">
         
         
-//         <div className="flex-grow rounded-lg border p-4">
-//         {messages.map((m, index) => (
+        <div className="flex-grow rounded-lg border p-4">
+        {messages.map((m, index) => (
               
-//               <p className="whitespace-pre-line" key={index}>
-//                 {m.content}
-//                 </p>
-//             ))}
+              <p className="whitespace-pre-line" key={index}>
+                {m.content}
+                </p>
+            ))}
 
-//         </div>
+        </div>
           
       
-//         <div className=" flex items-center p-2">
-//           <form className="flex-1" onSubmit={handleSubmit}>
-//             <Input placeholder="Type your message here..." value={input} onChange={handleInputChange}  />
-//           </form>
-//           <Button className="ml-2" type="submit" >
-//             Send
-//           </Button>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
+        <div className=" flex items-center p-2">
+          <form className="flex-1" onSubmit={handleSubmit}>
+            <Input placeholder="Type your message here..." value={input} onChange={handleInputChange}  />
+          </form>
+          <Button className="ml-2" type="submit" >
+            Send
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
